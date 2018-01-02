b5d0f2338006e48bf2c8c940a8fad3db7bdab1e1
machetazo <fabianmontero1998@gmail.com>
2018-01-02 16:28:54 -0600

    back to basics

--- index.js
+++ index.js
@@ -1,17 +1,15 @@
-const express = require('express') ,
-      cookieParser = require('cookie-parser') ,
-      bodyParser = require('body-parser') ,
-      app = express();
+var express = require('express')
+var app = express();
+var port = process.env.PORT || 3000
 
-const PORT = process.env.PORT || 5000
 
-app.use(express.logger('dev'));     /* 'default', 'short', 'tiny', 'dev' */
-app.use(bodyParser.urlencoded({
-extended: true
-}));
-
-app.use(bodyParser.json());
+/*
 app.use(express.static(__dirname + '/webpage'));
+*/
+app.get("/" , function(req , res){
+      res.send("Hello World");
+})
 
 
-app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
+app.listen(port);
