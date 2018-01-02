7afa4e8b0b42051c85e8fd55f446eeba5f4fe85a
machetazo <fabianmontero1998@gmail.com>
2018-01-02 17:37:24 -0600

    express

--- index.js
+++ index.js
@@ -3,13 +3,7 @@ var app = express();
 var port = process.env.PORT || 3000
 
 
-/*
 app.use(express.static(__dirname + '/webpage'));
-*/
-app.get("/" , function(req , res){
-      res.send("Hello World");
-})
-
 
 
 app.listen(port);
